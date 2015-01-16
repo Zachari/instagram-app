6f5c13461411352ab1d0104f369d54b748137012
Zachari LaCombe <zacharilouis@gmail.com>
2015-01-15 22:12:45 -0600

    Changed configurations

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@
  */
 
 module.exports = {
-    db: process.env.db || 'localhost',
+    db: process.env.db || 'mongodb://zachari:meghanboo3991@ds031641.mongolab.com:31641/lookingglass',
     clientSecret: process.env.clientSecret || 'd6f4c905e91c44d48e6a7c7b6d04f8fc',
     tokenSecret: process.env.tokenSecret || '4=3W5QbFXW)T1}o:|m]Kvg[EXgJvXN'
-}
+};
